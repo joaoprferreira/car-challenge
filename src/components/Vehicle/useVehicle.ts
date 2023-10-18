4adcecaec34f749c20e3234c3cf16d8d2b917ed2
João Pedro Ferreira <joaof082@gmail.com>
2023-10-17 23:10:30 -0300

    feat: building useVehicle to vehicles
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

---
+++ src/components/Vehicle/useVehicle.ts
@@ -0,0 +1,34 @@
+export default function useVehicle() {
+  function formatPrice(number = 0, currencySymbol = "R$") {
+    if (isNaN(number)) {
+      return "Valor inválido";
+    }
+    const numberString = number.toString();
+    const [integerPart, decimalPart = ""] = numberString.split(".");
+    const formattedIntegerPart = integerPart.replace(
+      /\B(?=(\d{3})+(?!\d))/g,
+      ","
+    );
+    return (
+      currencySymbol +
+      formattedIntegerPart +
+      (decimalPart ? "." + decimalPart : "")
+    );
+  }
+
+  const formatTimesTamp = (time: number) => {
+    const date = new Date(time * 1000);
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    const formattedDate = date.toLocaleString("pt-BR", options);
+    return formattedDate;
+  };
+
+  return { formatPrice, formatTimesTamp };
+}
