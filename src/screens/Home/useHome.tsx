2aebd85c0f4ba21dea6e02c9d568d0ab6508c1cb
João Pedro Ferreira <joaof082@gmail.com>
2023-10-18 14:37:21 -0300

    feat: building hook to home
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

---
+++ src/screens/Home/useHome.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function useHome() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return { openModal, closeModal, isModalOpen };
+}
